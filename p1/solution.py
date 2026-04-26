a18d30023ec9abc4a2eccc28ea227260dc214576
Shane Tucker <shanemtucker05@gmail.com>
2026-04-26 19:45:32 -0400

    potential submit

--- p1/solution.py
+++ p1/solution.py
@@ -1,1 +1,27 @@
 # Your solution goes here
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("p1/train.csv")
+dftest = pd.read_csv("p1/test.csv")
+N = len(df) #This and the next 2 lines calculate the inverted matrix in the normal equation
+sumxi = df["age"].sum()
+Xinv = np.linalg.inv(np.array([[N,sumxi],[sumxi,(df["age"]**2).sum()]]))
+sumyi = df["charges"].sum()
+sumxyi = (df["charges"]*df["age"]).sum()
+Y = np.array([[sumyi],[sumxyi]]) #This and the previous 2 lines helps find the second matrix in the normal equation with known values
+res = np.dot(Xinv,Y) #Multipllying these 2 matrices allows us to find the slope and y intercept
+b = res[0][0]
+m = res[1][0]
+Ntest = len(dftest)
+SSE = 0
+for i in range(1,Ntest+1): #Calculate sum of squared errors
+    SSE+=(dftest["charges"].iloc[i-1]-(m*dftest["age"].iloc[i-1])-b)**2
+print(f"Sum of squared errors: {SSE:.2f}")
+
+dfplot = pd.DataFrame({"pred":(m*dftest["age"]+b), "actual": dftest["charges"]}) #Create new dataframe with one column for the prediced charge based on our earlier code and one for the actual charge
+scat = dfplot.plot.scatter(title="Health Insurance Charges Predicted vs. Actual",x="pred",y="actual") #Plot testing data onto scatter plot
+scat.set_xticks([0,10000,20000,30000,40000,50000,60000])
+plt.savefig("p1/problem1.png")
+
--- p3/solution.py
+++ p3/solution.py
@@ -1,1 +1,51 @@
 # Your solution goes here
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+
+df = pd.read_csv("p3/Mall_Customers.csv")
+
+#Pre-processing including dropping irrelevant values and discretization
+df = df.drop(columns="CustomerID")
+df["Gender"] = df["Gender"].map({"Male": 0, "Female": 1})
+
+#Define import values such as list of number of clusters and array for training
+kvals = [2,3,5]
+X = np.array(df)
+bestSE = 99999999999
+SSElist = [-1,-1,-1]
+best_k = -1
+i=0
+
+#Train a new model for each number of clusters
+for k in kvals:
+    kmc = KMeans(k,random_state=0).fit(X)
+    SSE = kmc.inertia_ #Calculate SSE for the current cluster and compare to the lowest SSE found thus far
+    SSElist[i]=SSE
+    if SSE < bestSE: 
+        bestSE = SSE
+        best_k = k
+    i+=1
+print(f"Best k for SSE: {best_k} with SSE: {bestSE:.2f}")
+
+#Plot line chart
+plt.figure(1)
+plt.plot(kvals,SSElist)
+plt.title("Number of Clusters vs. SSE")
+plt.xlabel("# Clusters")
+plt.ylabel("SSE")
+plt.savefig("p3/problem3.png")
+
+#Plot scatter plot
+plt.figure(2)
+pca = PCA(2)
+X = pd.DataFrame(X)
+new_X = pca.fit_transform(X)
+plt.scatter(new_X[:,0],new_X[:,1],c=pd.Series(kmc.labels_).map({0: "blue", 1: "red", 2: "green", 3: "yellow", 4: "orange"}))
+plt.title("Scatter Plot of Data After Principle Component Analysis")
+plt.xlabel("PC 1")
+plt.xlabel("PC 2")
+plt.savefig("p3/clusters.png")
+
